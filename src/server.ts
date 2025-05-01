66d3c4e116b39ea0baca06c643f24104321c513b
Aryan Jagarwal <aryanjagarwal@Aryans-MacBook-Air.local>
2025-05-01 14:30:30 +0530

    npm package changed

--- src/server.ts
+++ src/server.ts
@@ -129,7 +129,7 @@ sequelize
     });
 
     // ✅ Start Server
-    const port = process.env.PORT || 5000;
+    const port = process.env.PORT || 7000;
     server.listen(port, () => {
       console.log(`✅ Server running on port ${port}`);
     });
